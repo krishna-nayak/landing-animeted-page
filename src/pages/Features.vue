7ebc224f5473a3472749b3e3ca8f3bfa13c52dfa
Krishna Nayak <nayak100krishna@gmail.com>
2021-06-27 11:13:00 +0530

    links

---
+++ src/pages/Features.vue
@@ -0,0 +1,7 @@
+<template>Features</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+    <AppBody />
+</template>
+
+<script>
+import AppBody from "../components/AppBody.vue";
+export default {
+    components: { AppBody },
+};
+</script>
+
+<style></style>
